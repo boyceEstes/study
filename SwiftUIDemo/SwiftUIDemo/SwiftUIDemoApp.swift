7603fda741069086abb77f0a2d357e662c849da2
Boyce Estes <estes.boyce@gmail.com>
2025-05-23 18:30:51 -0400

    adding swift project from swiftui demos

---
+++ SwiftUIDemo/SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Boyce Estes on 5/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
